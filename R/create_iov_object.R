779cfc2fc4d6925e857ee20b504b6d69e26998c0
ronkeizer <ronkeizer@gmail.com>
2017-08-04 13:59:31 -0700

    misc

--- R/create_iov_object.R
+++ R/create_iov_object.R
@@ -1,7 +1,7 @@
 #' Calculate the specs for including IOV, e.g. which elements should be fixed
 #' and the right size of the new omega matrix.
 #' Currently only working for a single parameter!
-#' 
+#'
 #' @param cv list of CVs of parameters
 #' @param omega omega matrix (lower triangle)
 #' @param data dataset
@@ -13,7 +13,7 @@
 #' @export
 create_iov_object <- function(cv = list(CL = 0.1),
                               omega = c(0.1),
-                              data = NULL, 
+                              data = NULL,
                               bins = c(0, 24, 48, 9999),
                               parameters = list(CL = 5),
                               fixed = NULL,
@@ -37,14 +37,14 @@ create_iov_object <- function(cv = list(CL = 0.1),
       fixed = fixed
     ))
   }
-  
+
   ## 1. get n from data / bins
   kappa <- c(paste0(names(cv)[1], "_kappa", 1:n))
   om1 <- omega
   om2 <- create_block_from_cv(cv = cv[[1]], n)
   om_new <- join_blocks(om1, om2)
   n_om <- lower_triangle_mat_size(om1)
-  
+
   ## reshuffle parameters
   iov_par <- !is.na(c(stringr::str_match(names(parameters), "_kappa")))
   if(! any(iov_par)) {
@@ -52,8 +52,8 @@ create_iov_object <- function(cv = list(CL = 0.1),
   }
   iov_list <- as.list(rep(0, length(kappa)))
   names(iov_list) <- kappa
-  
-  non_iov <- par[!iov_par]
+
+  non_iov <- parameters[!iov_par]
   new_par <- c(non_iov[1:n_om], iov_list, non_iov[(n_om+1):length(non_iov)])
   return(list(
     parameters = new_par,
