0c4917edf444fb38c76ab3a338be8ae78683019b
Ron Keizer <ron@insight-rx.com>
2021-07-09 21:14:59 +0000

    update namespace

--- R/PKPDmap-package.R
+++ R/PKPDmap-package.R
@@ -5,7 +5,6 @@
 #' @docType package
 #' @name PKPDmap
 #' @import mvtnorm PKPDsim bbmle Rcpp optimx
-#' @importFrom Rcpp sourceCpp
 #' @importFrom stats cov cov2cor filter glm lm predict.lm weights
 #' @importFrom utils setTxtProgressBar txtProgressBar tail
 #' @author Ron Keizer \email{ron@insight-rx.com}
