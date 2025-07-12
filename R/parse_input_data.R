e901b84d58fc722181f3f4694d36fd9b8d26aaac
Ron Keizer <ron@insight-rx.com>
2025-07-12 07:06:58 +0000

    merge fix

--- R/parse_input_data.R
+++ R/parse_input_data.R
@@ -7,6 +7,12 @@ parse_input_data <- function(
   cols = list(x = "t", y = "y"),
   obs_type_label = NULL
 ) {
+  ## Parse data to include label for obs_type
+  if(is.null(obs_type_label)) {
+    data$obs_type <- 1
+  } else {
+    data$obs_type <- data[[obs_type_label]]
+  }
   colnames(data) <- tolower(colnames(data))
   if(!all(tolower(unlist(cols)) %in% names(data))) {
     stop("Expected column names were not found in data. Please use 'cols' argument to specify column names for independent and dependent variable.")
@@ -17,12 +23,6 @@ parse_input_data <- function(
       data <- data[data$comp == "obs",]
       data$evid <- 0
     }
-  }
-  ## Parse data to include label for obs_type
-  if(is.null(obs_type_label)) {
-    data$obs_type <- 1
-  } else {
-    data$obs_type <- data[[obs_type_label]]
   }
   if("evid" %in% colnames(data)) {
     data <- data[data$evid == 0,]
