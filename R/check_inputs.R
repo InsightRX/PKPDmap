0d26ff4cb44c2f68927457a797db7c1584f05f91
Ron Keizer <ron@insight-rx.com>
2025-09-19 14:26:10 -0700

    add input check

--- R/check_inputs.R
+++ R/check_inputs.R
@@ -14,4 +14,13 @@ check_inputs <- function(model, data, parameters, omega, regimen, censoring, typ
   if(!("function" %in% class(model))) {
     stop("The 'model' argument requires a function, e.g. a model defined using the new_ode_model() function from the PKPDsim package.")
   }
+  if(!is.null(parameters)) {
+    defined_parameters <- names(attr(model, "parameters"))
+    if(! all(defined_parameters %in% names(parameters))) {
+      stop("One or more required parameters for the model have not been specified.")
+    }
+    if(any(names(parameters) %in% defined_parameters)) {
+      warning("One or more of the provided `parameters` are not supported by the model and will be ignored. Passing unknown parameters may affect IIV and IOV structure and result in erroneous output.")
+    }
+  }
 }
